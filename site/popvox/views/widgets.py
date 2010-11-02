07fb28f5a8d862bdc43d191494e33c930815d5da
POPVOX WWW (Josh) <josh@popvox.com>
2010-11-02 20:53:20 +0000

    widget issues

--- site/popvox/views/widgets.py
+++ site/popvox/views/widgets.py
@@ -7,9 +7,14 @@ from popvox.models import *
 from settings import SITE_ROOT_URL
 
 def bill_js(request):
+	try:
+		bill = None if not "bill" in request.GET else bill_from_url("/bills/us/" + request.GET["bill"])
+	except:
+		bill = None
+	
 	return render_to_response('popvox/widgets/bill.js', {
 		"siteroot": SITE_ROOT_URL,
-		"bill": get_object_or_404(Bill, id=request.GET["bill"]) if "bill" in request.GET else None
+		"bill": bill
 	}, context_instance=RequestContext(request))
 
 
