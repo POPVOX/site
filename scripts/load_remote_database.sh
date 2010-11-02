020182e91db8ed2b6c46b80f106a3b8075dcef1a
Joshua Tauberer <jt@occams.info>
2010-11-02 12:55:31 -0400

    updated load_remote_database.sh

--- scripts/load_remote_database.sh
+++ scripts/load_remote_database.sh
@@ -1,13 +1,13 @@
 export DEBUG=1
 export DONT_CREATE_USERPROFILES=1
 
-cd ../www_popvox_com
+cd ../site
 
-ssh popvox@occams "cd www_popvox_com; python manage.py dumpdata --format=xml --indent=2 auth registration popvox" > database.xml
+ssh www@popvox.com "cd sources/site; python manage.py dumpdata --settings=settings --indent=2 auth registration popvox trafficanalysis" > database.json
 
 rm database.sqlite
-python manage.py syncdb --noinput
+python manage.py syncdb --noinput --settings=settings
 
-#python manage.py flush --noinput
+#python manage.py flush --noinput --settings=settings
 
-python manage.py loaddata database.xml
+python manage.py loaddata --settings=settings database.json
