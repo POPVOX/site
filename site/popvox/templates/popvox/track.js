bd8e271415e816f10ef5b709575855caef57b674
Joshua Tauberer <jt@occams.info>
2010-12-03 10:35:13 -0500

    finishing the suggestions and hidden tabs of the leg staff dash

--- site/popvox/templates/popvox/track.js
+++ site/popvox/templates/popvox/track.js
@@ -14,21 +14,25 @@ function track(billid, track) {
 				if (track == "+") {
 					tracked_bills_changed = true;
 					if (ret.value == "+") {
-						elems.text("U");
+						elems.addClass("active");
+						elems.text("Un-Track This Bill");
 						elems.attr("title", "Un-Track This Bill");
 					} else {
-						elems.text("T");
+						elems.removeClass("active");
+						elems.text("Track This Bill");
 						elems.attr("title", "Track This Bill");
 					}
 				} else if (track == "-") {
 					if (ret.value == "-") {
 						antitracked_bills_added = true;
-						elems.text("S");
+						elems.addClass("active");
+						elems.text("Add Back to Suggestions");
 						elems.attr("title", "Add Back to Suggestions");
 						$(".bill[billid='" + billid + "']").hide(); //fadeOut is nice but only seems to work for instances of the bill that are currently on the screen
 					} else {
 						antitracked_bills_removed = true;
-						elems.text("H");
+						elems.removeClass("active");
+						elems.text("Hide From Suggestions");
 						elems.attr("title", "Hide From Suggestions");
 						$(".bill[billid='" + billid + "']").show();
 					}
