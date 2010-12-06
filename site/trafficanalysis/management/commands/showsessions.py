f4cfa4b307bc55f976a320431d4a53ca65cc21cc
Joshua Tauberer <jt@occams.info>
2010-12-06 10:49:26 -0500

    trafficanalysis: some extra tools to find particular sessions

--- site/trafficanalysis/management/commands/showsessions.py
+++ site/trafficanalysis/management/commands/showsessions.py
@@ -1,16 +1,42 @@
 from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
+
+import datetime
+
 from trafficanalysis.models import Session
 
 class Command(BaseCommand):
 	args = ''
 	help = 'Displays the most recent sessions.'
 	
+	option_list = BaseCommand.option_list + (
+		make_option('--date', help="Show sessions that extend around a given time."),
+		)
+	
 	def handle(self, *args, **options):
-		for session in reversed(Session.objects.all()[0:5]):
-			print session.user, session.start, "to", session.end
+		filter = { }
+		
+		if len(args) == 0:
+			if options["date"] != None:
+				filter["start__lte"] = options["date"]
+				filter["end__gte"] = options["date"]
+		
+			for session in reversed(Session.objects.filter(**filter)):
+				print session.user if session.user != None else "#"+str(session.id), session.start, "to", session.end
+				for pe in session.get_path():
+					path = getattr(pe, "path", "")
+					view = getattr(pe, "view", "")
+					goal = getattr(pe, "goal", "")
+					print "", pe.time, path, view, goal
+
+		else:
+			
+			session = Session.objects.get(id=args[0])
+			print session.user if session.user != None else "#"+str(session.id), session.start, "to", session.end
+			print session.get_ua()
 			for pe in session.get_path():
 				path = getattr(pe, "path", "")
 				view = getattr(pe, "view", "")
 				goal = getattr(pe, "goal", "")
-				print "", pe.time, path, view, goal
-
+				rcode = getattr(pe, "rcode", "")
+				print "", pe.time, path, view, goal, rcode
