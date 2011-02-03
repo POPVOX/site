2923c9b55245cf0eaf5ecbc7557eb1cc68e32a01
POPVOX WWW (Josh) <josh@popvox.com>
2011-02-03 15:13:33 +0000

    adserver: revise the adserver_trail session variable to be a dict rather than a list

--- site/adserver/adselection.py
+++ site/adserver/adselection.py
@@ -15,17 +15,14 @@ from settings import SITE_ROOT_URL
 def select_banner(adformat, targets, ad_trail):
 	# Select a banner to show and return the banner and the display CPM and CPC prices.
 	
+	now = datetime.now()
+
 	banners = adformat.banners.filter(active=True, order__active=True) \
 		.select_related("order") \
 		.order_by() # clear default ordering which loads up the Advertiser object
 		
 	target_ids = [t.id for t in targets]
 
-	ad_trail_dict = { }
-	if ad_trail != None:
-		for banner_id, last_display_time in ad_trail:
-			ad_trail_dict[banner_id] = last_display_time
-	
 	# Remove banners that do not match the targetting.
 	def matches_targets(banner):
 		for targetgroup in banner.order.targets_parsed():
@@ -77,13 +74,13 @@ def select_banner(adformat, targets, ad_trail):
 		remnant = banner.order.advertiser.remnant
 		
 		# Check the ad frequency against the time of the last display.
-		# Remnant ads should never appear in ad_trail_dict but we'll check anyway.
-		if banner.id in ad_trail_dict and not remnant:
+		# Remnant ads should never appear in ad_trail but we'll check anyway.
+		if banner.id in ad_trail and not remnant:
 			if banner.order.period != None:
 				period = timedelta(hours=banner.order.period)
 			else:
 				period = timedelta(seconds=20)
-			if datetime.now() - ad_trail_dict[banner.id] < period:
+			if now - ad_trail[banner.id] < period:
 				# clear field and continue looking for a banner
 				banner = None
 				continue
@@ -217,6 +214,8 @@ def select_banner(adformat, targets, ad_trail):
 
 def show_banner(format, request, context, targets, path):
 	# Select a banner to show and return the HTML code.
+
+	now = datetime.now()
 	
 	# Don't show ads when the user agent is a bot.
 	if not "HTTP_USER_AGENT" in request.META:
@@ -227,11 +226,14 @@ def show_banner(format, request, context, targets, path):
 		
 	# Prepare the list of ads we've served to this user recently. Prune the list
 	# of ads not seen in two days, which is the maximum.
+	adserver_trail = None
 	if hasattr(request, "session"):
-		if not "adserver_trail" in request.session:
-			request.session["adserver_trail"] = []
-		request.session["adserver_trail"] = [t for t in request.session["adserver_trail"]
-			if datetime.now() - t[1] < timedelta(days=2)]
+		if not "adserver_trail" in request.session or type(request.session["adserver_trail"]) == list:
+			request.session["adserver_trail"] = { }
+		adserver_trail = request.session["adserver_trail"]
+		for bannerid, last_impression_date in adserver_trail.items():
+			if now - last_impression_date > timedelta(days=2):
+				del adserver_trail[bannerid]
 	
 	# Besides the targets specified in the template tag, additionally apply
 	# templates stored in the session key and context variable
@@ -250,7 +252,7 @@ def show_banner(format, request, context, targets, path):
 		targets += [make_target2(t) for t in context["adserver-targets"]]
 	
 	# Find the best banner to run here.
-	selection = select_banner(format, targets, request.session["adserver_trail"] if hasattr(request, "session") else None)
+	selection = select_banner(format, targets, adserver_trail)
 	if selection == None:
 		return Template(format.fallbackhtml).render(context)
 	
@@ -264,7 +266,7 @@ def show_banner(format, request, context, targets, path):
 	imb, isnew = ImpressionBlock.objects.get_or_create(
 		banner = b,
 		path = sp,
-		date = datetime.now().date,
+		date = now.date,
 		)
 
 	# Atomically update the rest.
@@ -288,11 +290,11 @@ def show_banner(format, request, context, targets, path):
 	im.save()
 	
 	# Clear out old impressions (maybe do this not quite so frequently?).
-	Impression.objects.filter(created__lt = datetime.now()-timedelta(minutes=30)).delete()
+	Impression.objects.filter(created__lt = now-timedelta(minutes=30)).delete()
 	
 	# Record that this ad was shown.
-	if hasattr(request, "session") and not b.order.advertiser.remnant:
-		request.session["adserver_trail"].append( (b.id, datetime.now()) )
+	if adserver_trail != None and not b.order.advertiser.remnant:
+		adserver_trail[b.id] = now
 	
 	# Parse the template. If the banner has HTML override code, use that instead.
 	if b.html != None and b.html.strip() != "":
