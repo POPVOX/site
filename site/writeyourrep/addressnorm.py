e018b9567c6e31de30ad786aef0c9f028af1e454
Joshua Tauberer <jt@occams.info>
2011-02-05 00:57:13 -0500

    test code for address normalization

--- site/writeyourrep/addressnorm.py
+++ site/writeyourrep/addressnorm.py
@@ -5,7 +5,11 @@ from settings import CDYNE_LICENSE_KEY
 
 from popvox.govtrack import stateapportionment
 
+last_response = None
+
 def verify_adddress(address):
+	global last_response
+	
 	req = urllib2.Request("http://pav3.cdyne.com/PavService.svc/VerifyAddressAdvanced",
 		json.dumps(
 		{
@@ -30,6 +34,8 @@ def verify_adddress(address):
 	except Exception, e:
 		raise ValueError("Address verification is not working at the moment.")
 	
+	last_response = ret
+	
 	if ret["ReturnCode"] not in (100, 101, 102, 103):
 		raise ValueError("The address was not found.")
 	
@@ -43,7 +49,9 @@ def verify_adddress(address):
 		if hasattr(address, a) and getattr(address, a).lower() != ret[b].lower():
 			setattr(address, a, ret[b])
 	
-	if ret["LegislativeInfo"]["CongressionalDistrictNumber"] == "AL":
+	if ret["LegislativeInfo"]["CongressionalDistrictNumber"] == "AL": # hmm
+		address.congressionaldistrict = 0
+	elif stateapportionment[address.state] == 1: # at-large comes back as 98, etc.
 		address.congressionaldistrict = 0
 	else:
 		address.congressionaldistrict = int(ret["LegislativeInfo"]["CongressionalDistrictNumber"])
@@ -51,8 +59,36 @@ def verify_adddress(address):
 	address.state_legis_lower = ret["LegislativeInfo"]["StateLegislativeLower"]
 	address.latitude = float(ret["GeoLocationInfo"]["AvgLatitude"])
 	address.longitude = float(ret["GeoLocationInfo"]["AvgLongitude"])
-	
-	# at-large comes back as 98, etc.
-	if stateapportionment[address.state] == 1:
-		address.congressionaldistrict = 0
 
+
+if __name__ == "__main__":
+	# DEBUG=1 DJANGO_SETTINGS_MODULE=settings PYTHONPATH=. python writeyourrep/addressnorm.py
+	
+	from popvox.models import PostalAddress
+	address = PostalAddress()
+	
+	import sys
+	address.address1 = sys.stdin.readline().strip()
+	city_state_zip = sys.stdin.readline().strip()
+	address.city = city_state_zip.split(",")[0].strip()
+	address.state = city_state_zip.split(",")[1].split(" ")[0].strip()
+	address.zipcode = city_state_zip.split(",")[1].split(" ")[1].strip()
+	
+	try:
+		verify_adddress(address)
+	except:
+		pass
+	
+	print "-------"
+	
+	print last_response
+	
+	print
+	
+	print address.address1
+	print address.city + ",", address.state, address.zipcode
+	print "CD", address.congressionaldistrict
+	print "SLDU", address.state_legis_upper
+	print "SLDL", address.state_legis_lower
+	print "Coord", address.latitude, address.longitude
+	
