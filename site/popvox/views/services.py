dfb904fd535a218b71f00a6906b526fe0fccc43b
William Donnell <william@popvox.com>
2011-04-12 14:28:34 +0000

    some work on the comment stream widget

--- site/popvox/views/services.py
+++ site/popvox/views/services.py
@@ -3,7 +3,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext, TemplateDoesNotExist
 
 from popvox.models import *
-from popvox.govtrack import statelist, CURRENT_CONGRESS
+from popvox.govtrack import statelist, statenames, CURRENT_CONGRESS
 
 from widgets import do_not_track_compliance
 
@@ -21,12 +21,14 @@ def widget_config(request):
 def widget_render(request, widgettype, account_key=None, widgetconfig_id=None):
 	comments = UserComment.objects.filter(message__isnull=False, status__in=(UserComment.COMMENT_NOT_REVIEWED, UserComment.COMMENT_ACCEPTED)).order_by("-created")
 	
-	title = ["Recent Comments"]
+	title1 = "Recent Comments"
+	title2 = "in POPVOX Nation"
+
 	show_bill_number = True
 	
 	if "state" in request.GET:
 		comments = comments.filter(state=request.GET["state"])
-		title.append("in " + request.GET["state"])
+		title2 = "in " + statenames[request.GET["state"]]
 
 	cx = []
 	
@@ -48,11 +50,13 @@ def widget_render(request, widgettype, account_key=None, widgetconfig_id=None):
 			
 		if len(cx) == 1: # the bills have to be processed first
 			show_bill_number = False
-			title[0] = "Comments on " + b.displaynumber()
+			title1 = "Recent Comments on"
+			title2 = b.displaynumber()
 
 	if "issue" in request.GET:
 		cx.append(comments.filter(bill__topterm__id=request.GET["issue"]))
-		title[0] = "Comments on " + IssueArea.objects.get(id=request.GET["issue"]).name
+		title1 = "Recent Comments on"
+		title2 = IssueArea.objects.get(id=request.GET["issue"]).name
 
 	if len(cx) > 0:
 		comments = cx[0]
@@ -62,7 +66,8 @@ def widget_render(request, widgettype, account_key=None, widgetconfig_id=None):
 	comments = comments[0:25]
 	
 	return render_to_response('popvox/widgets/' + widgettype + '.html', {
-		'title': " ".join(title),
+		'title1': title1,
+		'title2': title2,
 		'comments': comments,
 		"show_bill_number": show_bill_number,
 		}, context_instance=RequestContext(request))
