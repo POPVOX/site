3a3bda6c358a67b0d93ff55f06be0fe1a34cb2af
Joshua Tauberer <jt@occams.info>
2011-05-03 07:36:57 -0400

    server sysinit; indexes

--- site/popvox/sql/usercomment.mysql.sql
+++ site/popvox/sql/usercomment.mysql.sql
@@ -1,4 +1,7 @@
-alter table popvox_usercomment add index bill_district(bill_id, state, congressionaldistrict, created);
-alter table popvox_usercomment add index bill_state(bill_id, state, created);
-alter table popvox_usercomment add index district(state, congressionaldistrict, created);
+ALTER TABLE popvox_usercomment ADD INDEX bill_district(bill_id, state, congressionaldistrict, created);
+ALTER TABLE popvox_usercomment ADD INDEX bill_state(bill_id, state, created);
+ALTER TABLE popvox_usercomment ADD INDEX district(state, congressionaldistrict, created);
+ALTER TABLE popvox_usercomment ADD INDEX popvox_usercomment_created(created);
+ALTER TABLE popvox_usercomment ADD INDEX popvox_usercomment_state_created(state, created);
+ALTER TABLE popvox_usercomment ADD INDEX popvox_usercomment_state_district_created(state, congressionaldistrict, created);
 
