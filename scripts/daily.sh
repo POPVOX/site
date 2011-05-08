95c769e2ec4170b25f45c9943990713e18e3a292
POPVOX WWW (Josh) <josh@popvox.com>
2011-05-08 17:28:09 +0000

    add manage.py cleanup to clear expired sessions, tweak top bills widget

--- scripts/daily.sh
+++ scripts/daily.sh
@@ -10,6 +10,7 @@ $SCRIPTS/update_fans.py
 #python popvox/send_mass_emails.py survey
 python popvox/update_bill_metadata.py
 python popvox/compute_prompts.py
+./manage cleanup
 
 cd /mnt/persistent
 $SCRIPTS/backup.sh
