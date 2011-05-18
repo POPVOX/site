49dc9128654510a9e12e14285a12d5f25278102a
Josh Tauberer <josh@popvox.com>
2011-05-18 21:52:41 +0000

    synreq intelligence play

--- site/writeyourrep/syns.py
+++ site/writeyourrep/syns.py
@@ -2,22 +2,30 @@
 
 from writeyourrep.send_message import Synonym, SynonymRequired
 
-OTHER = ("other", "others", "miscellaneous", "other - not listed", "optionally select an issue", "general concerns")
-
-# For each SynonymRequired...
-for s in SynonymRequired.objects.all():
-	term1 = s.term1set.strip()
-	term2set = s.term2set.strip().split("\n")
+def do_test(term1set, term2set):
+	# Find all existing mappings for the first term.
 	counters = { }
-	# ...find all other left-hand-side terms that share a common right-hand-side term with this term...
-	for s2 in Synonym.objects.filter(term1=term1).exclude(term2__in=OTHER):
-		for s3 in Synonym.objects.filter(term2=s2.term2).exclude(term1=term1).exclude(term1__startswith="#"):
+	for s1 in Synonym.objects.filter(term1=term1set[0], last_resort=False, auto=False):
+		# Find other terms that map to the same right-hand term, besides of course the original term.
+		for s2 in Synonym.objects.filter(term2=s1.term2, last_resort=False, auto=False).exclude(term1=term1set[0]):
 			# ...and see if any of those map to one of the right-hand-side options in this SynonymRequired.
-			for s4 in Synonym.objects.filter(term1=s3.term1).exclude(term2__in=OTHER):
-				if s4.term2 in term2set:
-					if not s4.term2 in counters: counters[s4.term2] = 0
-					counters[s4.term2] += 1
-					#print term1, "(", s3, ")",  s4.term2
+			for s3 in Synonym.objects.filter(term1=s2.term1, last_resort=False, auto=False):
+				if s3.term2 in term2set:
+					if not s3.term2 in counters: counters[s3.term2] = 0
+					counters[s3.term2] += 1
 	if len(counters) > 0:
-		print term1.encode("utf8"), unicode(counters).encode("utf8")
+		value, mx = None, 0
+		for k, v in counters.items():
+			if v > mx and v > 5:
+				value = k
+				mx = v
+		if value:
+			print repr(term1set), value.encode("utf8")
+
+# Do a test on each SynonymRequired...
+for s in SynonymRequired.objects.all():
+	term1set = s.term1set.strip().split("\n")
+	term2set = s.term2set.strip().split("\n")
+	do_test(term1set, term2set)
+
 
