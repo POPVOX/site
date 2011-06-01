fb812f91fd10f0ccf1341fab1dd542c50bd1d84b
Josh Tauberer <josh@popvox.com>
2011-06-01 00:23:54 +0000

    fix shorturl middleware to respect request.session set explitily to None

--- site/shorturl/middleware.py
+++ site/shorturl/middleware.py
@@ -1,6 +1,6 @@
 class ShorturlMiddleware:
 	def process_response(self, request, response):
-		if not getattr(request, "just_added_shorturl", False) and hasattr(request, "session"):
+		if not getattr(request, "just_added_shorturl", False) and getattr(request, "session", None) != None:
 			if "shorturl" in request.session:
 				del request.session["shorturl"]
 		return response
