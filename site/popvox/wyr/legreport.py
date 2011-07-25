42980f3754d9660c036bc15496cfb29bea3d9f18
Josh Tauberer <josh@popvox.com>
2011-07-25 23:51:38 +0000

    legreport email and media (JL)

--- site/popvox/wyr/legreport.py
+++ site/popvox/wyr/legreport.py
@@ -101,5 +101,5 @@ def generate_report(recipients, office, report_start_date, report_end_date, down
 	msg.send()
 
 if __name__ == "__main__":	
-	generate_report(["josh@joshmlewis.com", "josh@popvox.com", "tauberer@gmail.com"], "PA-H02", date(2011, 06, 01), date(2015, 06, 19), True)
+	generate_report(["josh@joshmlewis.com","joshlewis@g.popvox.com"], "PA-H02", date(2011, 06, 01), date(2015, 06, 19), True)
 
