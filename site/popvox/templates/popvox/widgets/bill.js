98eed1c35e40b7a5c17acce896699bce822b0c7d
Josh Tauberer <josh@popvox.com>
2011-07-29 19:20:04 +0000

    revise bill widget so that when there are no stats yet, display the same as when the bill reference is invalid, just a bubble, not 'go first'

--- site/popvox/templates/popvox/widgets/bill.js
+++ site/popvox/templates/popvox/widgets/bill.js
@@ -4,6 +4,6 @@
 	{% bill_statistics bill as stats %}
 {% endif %}
 
-document.write('<style>.popvoxwidget { width: 162px; height: 47px; background-image: url(https://www.popvox.com/media/icons/speak_up_{% if bill %}ext{% else %}bubble{% endif %}.png); cursor: pointer; text-align: left } {% if not bill %}.popvoxwidget:hover { background-position: left bottom }{% endif %}</style>');
-document.write('<div class="popvoxwidget" onclick="document.location=\'{{siteroot|escapejs}}{% if bill %}{{bill.url|escapejs}}{% endif %}\'"><div style="position: relative; line-height: 17px; top: 6px; left: 26px; color: white; font-family: sans-serif; font-size: 12px; width: 50px">{% if stats %}{{stats.pro_pct}}%<br/>{{stats.con_pct}}%{% endif %}{% if bill and not stats %}<div style="padding-top: 1px">Go<br/>first.</div>{% endif %}</div></div>')
+document.write('<style>.popvoxwidget { width: 162px; height: 47px; background-image: url(https://www.popvox.com/media/icons/speak_up_{% if bill and stats %}ext{% else %}bubble{% endif %}.png); cursor: pointer; text-align: left } {% if not bill %}.popvoxwidget:hover { background-position: left bottom }{% endif %}</style>');
+document.write('<div class="popvoxwidget" onclick="document.location=\'{{siteroot|escapejs}}{% if bill %}{{bill.url|escapejs}}{% endif %}\'"><div style="position: relative; line-height: 17px; top: 6px; left: 26px; color: white; font-family: sans-serif; font-size: 12px; width: 50px">{% if stats %}{{stats.pro_pct}}%<br/>{{stats.con_pct}}%{% endif %}</div></div>')
 
