507b1ba928fd9ea496cbad42b328d47989f3b499
Josh Tauberer <josh@popvox.com>
2011-09-14 01:38:23 +0000

    update mysql_shell.sh to work on a slave server

--- site/mysql_shell.sh
+++ site/mysql_shell.sh
@@ -1,2 +1,11 @@
-mysql -u root -p"qsg;5TtC" popvox "$@"
+#!/bin/bash
+
+MYUSER=root
+REMOTEDB=localhost
+if [ -f ~/slave ]; then
+	source ~/slave
+	MYUSER=slave
+fi
+
+mysql -u $MYUSER -h $REMOTEDB -p"qsg;5TtC" popvox "$@"
 
