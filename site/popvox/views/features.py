f5089b9364758e1094a1ade989ecc22012102b45
Joshua Tauberer <josh@popvox.com>
2011-10-20 19:35:23 +0000

    first pass at supercommittee feature page

---
+++ site/popvox/views/features.py
@@ -0,0 +1,68 @@
+from django.http import Http404, HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.core.cache import cache
+
+from popvox.models import Bill
+from popvox.views.bills import bill_statistics, get_default_statistics_context
+
+def supercommittee(request):
+	bill_list = [
+		{
+			"bill": Bill.objects.get(id=19773),
+			"savings": 453,
+			"category": "Tax",
+		},
+		{
+			"bill": Bill.objects.get(id=19774),
+			"savings": 106,
+			"category": "Tax",
+		},
+		{
+			"bill": Bill.objects.get(id=19775),
+			"savings": 410,
+			"category": "Tax",
+		},
+		{
+			"bill": Bill.objects.get(id=19776),
+			"savings": 240,
+			"category": "Health",
+		},
+		{
+			"bill": Bill.objects.get(id=19777),
+			"savings": 17,
+			"category": "Health",
+		},
+		{
+			"bill": Bill.objects.get(id=19778),
+			"savings": 73,
+			"category": "Defense",
+		},
+		{
+			"bill": Bill.objects.get(id=19357),
+			"savings": 100,
+			"category": "Government Reform",
+			"title": "Reducing the Size of the Federal Government Through Attrition",
+			"description": "Reduces the total number of federal employees by 10 percent over a five year period, lowering the deficit by $100 billion over 10 years. This is a proposal the Supercommittee may be considering and was proposed by Americans for Prosperity."
+		},
+		{
+			"bill": Bill.objects.get(id=19780),
+			"savings": 166,
+			"category": "Financial Services",
+		},
+	]
+
+	for bill in bill_list:
+		if not "title" in bill:
+			bill["title"] = bill["bill"].nicename
+		if not "description" in bill:
+			bill["description"] = bill["bill"].description
+		bill["sentiment"] = bill_statistics(bill["bill"], "POPVOX Nation", "POPVOX Nation")
+		if bill["sentiment"]:
+			bill["sentiment"]["scaled_pro"] = 150 * bill["sentiment"]["pro_pct"] / 100
+			bill["sentiment"]["scaled_con"] = 150 - bill["sentiment"]["scaled_pro"]
+	
+	return render(request, "popvox/features/supercommittee.html",
+		{
+			"bills": bill_list
+		})
+
