874169fc5588120f4819ee1929211cf87731c598
Josh Tauberer <josh@popvox.com>
2011-11-18 02:45:10 +0000

    new scraper server IP address

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,3 +1,3 @@
 # To be run from /mnt/persistent
-rsync -az --delete --delete-excluded 10.112.57.60::govtrackdata data/govtrack --exclude "us/bills.text*" --exclude rdf --exclude "**/repstats" --exclude "**/repstats.person" --exclude "**/index.*" --exclude "us/gis" --exclude "us/fec" --exclude "us/*/cr" --exclude "**/gen.*" --exclude "**/bills.cbo" --exclude "**/bills.ombsap" --exclude "**/stats" --exclude misc/database.sql.gz
+rsync -az --delete --delete-excluded 10.122.105.72::govtrackdata data/govtrack --exclude "us/bills.text*" --exclude rdf --exclude "**/repstats" --exclude "**/repstats.person" --exclude "**/index.*" --exclude "us/gis" --exclude "us/fec" --exclude "us/*/cr" --exclude "**/gen.*" --exclude "**/bills.cbo" --exclude "**/bills.ombsap" --exclude "**/stats" --exclude misc/database.sql.gz
 
