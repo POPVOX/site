POPVOX/site

commit 152bc93726e61c762a0e61dc619bc7a240b0efd2

for pv admins, add widget owner breakdown to bill report

site/popvox/models.py
@@ -1148,6 +1148,7 @@ class UserComment(models.Model):
 	METHOD_SITE = 0
 	METHOD_CUSTOMIZED_PAGE = 1
 	METHOD_WIDGET = 2
+	METHOD_NAMES = { METHOD_SITE: "POPVOX.com", METHOD_CUSTOMIZED_PAGE: "PV.com Customized Landing Page", METHOD_WIDGET: "Write Congress Widget" }
 
 	user = models.ForeignKey(User, related_name="comments", db_index=True, on_delete=models.CASCADE) # user authoring the comment
 	bill = models.ForeignKey(Bill, related_name="usercomments", db_index=True, on_delete=models.PROTECT)
@@ -1380,7 +1381,7 @@ class UserComment(models.Model):
 class UserCommentReferral(models.Model):
 	# This class is used to avoid cascaded deletes on UserComment objects
 	# if a referring object is deleted.
-	comment = models.ForeignKey(UserComment, on_delete=models.CASCADE) # implicitly indexed by the unique_together
+	comment = models.ForeignKey(UserComment, related_name="referrers",  on_delete=models.CASCADE) # implicitly indexed by the unique_together
 	referrer_content_type = models.ForeignKey(ContentType, db_index=True, on_delete=models.CASCADE)
 	referrer_object_id = models.PositiveIntegerField(db_index=True)
 	referrer = generic.GenericForeignKey('referrer_content_type', 'referrer_object_id')

site/popvox/views/bills.py
@@ -1769,6 +1769,29 @@ def billreport_getinfo(request, congressnumber, billtype, billnumber, vehicleid)
 			by_town[rec["address__city"]][rec["position"]] = rec["count"]
 		# Sort.
 		by_town = sorted(by_town.values(), key=lambda x : x["name"].lower())
+		
+	# For admins, show a breakdown by source.
+	by_source = None
+	if request.user.is_superuser or request.user.is_staff:
+		by_source = { }
+		for rec in bill_comments(bill, state=state, congressionaldistrict=district)\
+			.values("method", "referrers__referrer_content_type", "referrers__referrer_object_id")\
+			.annotate(count=Count('id'))\
+			.order_by():
+				
+			# convert dict of content type and obj id into object
+			method_name = UserComment.METHOD_NAMES[rec["method"]]
+			if rec["referrers__referrer_content_type"] == None:
+				referrer = method_name
+			else:
+				from django.contrib.contenttypes.models import ContentType
+				referrer = ContentType.objects.get(id=rec["referrers__referrer_content_type"]).get_object_for_this_type(id=rec["referrers__referrer_object_id"])
+				referrer = unicode(referrer)
+				referrer += " (" + method_name + ")"
+			
+			by_source[referrer] = rec["count"]
+		by_source = sorted(by_source.items(), key=lambda x : -x[1]) # Sort
+		
 			
 	# Return.
 	
@@ -1822,6 +1845,7 @@ def billreport_getinfo(request, congressnumber, billtype, billnumber, vehicleid)
 				congressionaldistrict=district)
 					if state != None and district not in (None, 0) else None,
 			"by_town": by_town,
+			"by_source": by_source,
 		}
 	}
 

site/popvox/templates/popvox/bill_report.html
@@ -22,23 +22,23 @@ h2 span {
 .tag_cloud .line {
 	padding-bottom: 3px;
 	}
-#by_town {
+.tabular_stats {
 	padding-top: 10px;
 	background: url(/media/master/bg_hr.png) left top repeat-x;
 	}
-	#by_town h4 {
+	.tabular_stats h4 {
 		font-weight: bold;
 		font-size: 13px;
 		}
-	#by_town h4 small {
+	.tabular_stats h4 small {
 		font-weight: normal;
 		font-size: 11px;
 		}
-	#by_town table th, #by_town table td {
+	.tabular_stats table th, .tabular_stats table td {
 		font-size: 12px;
 		padding-right: 1em;
 		}
-	#by_town .container {
+	.tabular_stats .container {
 		height: 8em;
 		overflow: auto;
 		}
@@ -310,21 +310,28 @@ h2 span {
 					$('#map').hide();
 					if (res.stats.overall) $('#showmap').show();
 				}
-
-				if (!res.stats.by_town || !res.stats.by_town.length) {
-					$('#by_town').hide();
-				} else {
-					$('#by_town').show();
-					var tbody = $('#by_town').find("tbody");
-					tbody.text("");
-					for (var i = 0; i < res.stats.by_town.length; i++) {
-						var row = $("<tr/>");
-							row.append($("<td/>").text(res.stats.by_town[i].name))
-							row.append($("<td style='text-align: right'/>").text(res.stats.by_town[i]["+"]))
-							row.append($("<td style='text-align: right'/>").text(res.stats.by_town[i]["-"]))
-						tbody.append(row);
+				
+				function fill_table(elemid, dataarray, colnames) {
+					if (!dataarray || !dataarray.length) {
+						$(elemid).hide();
+					} else {
+						$(elemid).show();
+						var tbody = $(elemid).find("tbody");
+						tbody.text("");
+						for (var i = 0; i < dataarray.length; i++) {
+							var row = $("<tr/>");
+							for (var j = 0; j < colnames.length; j++) {
+								var cell = $("<td/>").text(dataarray[i][colnames[j]]);
+								if (j > 0) cell.attr("style", "text-align: right");
+								row.append(cell);
+							}
+							tbody.append(row);
+						}
 					}
 				}
+
+				fill_table("#by_town", res.stats.by_town, ["name", "+", "-"])
+				fill_table("#by_source", res.stats.by_source, [0, 1])
 			}
 		});
 
@@ -526,7 +533,7 @@ h2 span {
 				</div>
 				{% endfor %}
 					
-				<div id="by_town" style="display: none">
+				<div id="by_town" style="display: none" class="tabular_stats">
 					<h4>By Address <small>(shown to legislative staff only in your state/district)</small></h4>
 					<div class="container">
 					<table>
@@ -539,6 +546,19 @@ h2 span {
 					</div>
 				</div>
 				
+				<div id="by_source" style="display: none" class="tabular_stats">
+					<h4>By Source <small>(shown to POPVOX admins only)</small></h4>
+					<div class="container">
+					<table>
+						<thead>
+							<tr><th>Source / Widget Owner</th> <th>Count (Positions)</th></tr>
+						</thead>
+						<tbody>
+						</tbody>
+					</table>
+					</div>
+				</div>
+
 				<a href="#" onclick="$('#map').show(); $('#showmap').hide(); return false;">
 					<div id="showmap" style="text-align: center; margin: .5em; display: none">Show National Map</div>
 				</a>

commit 0700bbe30d80c9e4b51e4cd6c02d44ee7f034f60

name clash caused by 152bc93726e61c762a0e61dc619bc7a240b0efd2

site/popvox/models.py
@@ -1381,7 +1381,7 @@ class UserComment(models.Model):
 class UserCommentReferral(models.Model):
 	# This class is used to avoid cascaded deletes on UserComment objects
 	# if a referring object is deleted.
-	comment = models.ForeignKey(UserComment, related_name="referrers",  on_delete=models.CASCADE) # implicitly indexed by the unique_together
+	comment = models.ForeignKey(UserComment, on_delete=models.CASCADE) # implicitly indexed by the unique_together
 	referrer_content_type = models.ForeignKey(ContentType, db_index=True, on_delete=models.CASCADE)
 	referrer_object_id = models.PositiveIntegerField(db_index=True)
 	referrer = generic.GenericForeignKey('referrer_content_type', 'referrer_object_id')

site/popvox/templates/popvox/widgets/writecongress_followup.html
@@ -3,10 +3,10 @@
 	<div id="writecongress_followup" class="colorbox" style="width: 500px">
 		<h1>Set Up A POPVOX Account</h1>
 
-		{% if comment.referrers.all|length == 0 %}
+		{% if comment.usercommentreferral_set.all|length == 0 %}
 			<h2>Thank you for using POPVOX&rsquo;s Write Congress tool to voice your comment.</h2>
 		{% else %}
-			<h2>{{comment.referrers.all.0.referrer}} is using POPVOX to help get your letter to Congress.</h2>
+			<h2>{{comment.usercommentreferral_set.all.0.referrer}} is using POPVOX to help get your letter to Congress.</h2>
 		{% endif %}
 		
 		<p>Setting up a POPVOX account lets you see when your comment is delivered, track the status of the bill and see what the rest of the nation thinks.</p>

site/popvox/views/bills.py
@@ -1776,17 +1776,17 @@ def billreport_getinfo(request, congressnumber, billtype, billnumber, vehicleid)
 	if request.user.is_superuser or request.user.is_staff:
 		by_source = { }
 		for rec in bill_comments(bill, state=state, congressionaldistrict=district)\
-			.values("method", "referrers__referrer_content_type", "referrers__referrer_object_id")\
+			.values("method", "usercommentreferral__referrer_content_type", "usercommentreferral__referrer_object_id")\
 			.annotate(count=Count('id'))\
 			.order_by():
 				
 			# convert dict of content type and obj id into object
 			method_name = UserComment.METHOD_NAMES[rec["method"]]
-			if rec["referrers__referrer_content_type"] == None:
+			if rec["usercommentreferral__referrer_content_type"] == None:
 				referrer = method_name
 			else:
 				from django.contrib.contenttypes.models import ContentType
-				referrer = ContentType.objects.get(id=rec["referrers__referrer_content_type"]).get_object_for_this_type(id=rec["referrers__referrer_object_id"])
+				referrer = ContentType.objects.get(id=rec["usercommentreferral__referrer_content_type"]).get_object_for_this_type(id=rec["usercommentreferral__referrer_object_id"])
 				referrer = unicode(referrer)
 				referrer += " (" + method_name + ")"
 			