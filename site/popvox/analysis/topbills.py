7dd33cccfb2a5271a0a09cb0b4f8ba2e584ba48d
Joshua Tauberer <josh@popvox.com>
2011-12-28 12:45:00 +0000

    add some columns to top-bills report

--- site/popvox/analysis/topbills.py
+++ site/popvox/analysis/topbills.py
@@ -4,16 +4,18 @@ import csv
 
 from django.db.models import Count
 
-from popvox.models import Bill
+from popvox.models import Bill, UserComment
 from popvox.govtrack import CURRENT_CONGRESS
 
 out = csv.writer(open("top_bills.csv", "w"))
-out.writerow(['comments', 'bill', 'issue'])
+out.writerow(['comments', 'bill', 'sponsor', 'issue', 'supporting', 'opposing'])
 
 for b in Bill.objects.filter(congressnumber=CURRENT_CONGRESS)\
 	.annotate(count=Count('usercomments'))\
 	.order_by('-count')\
 	.select_related("topterm")\
 	[0:100]:
-	out.writerow([str(b.count), b.title.encode("utf8"), unicode(b.topterm)])
+	csup = UserComment.objects.filter(bill=b, position="+").count()
+	copp = UserComment.objects.filter(bill=b, position="-").count()
+	out.writerow([str(b.count), b.title.encode("utf8"), b.sponsor.name().encode("utf8") if b.sponsor else "", b.topterm.name.encode("utf8") if b.topterm else "", csup, copp])
 
