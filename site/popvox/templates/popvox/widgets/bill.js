273b862e7acaa1c6beebe68259f66fe062bacef8
Josh Tauberer <josh@popvox.com>
2011-12-29 16:41:12 +0000

    fix bill button html so it is more likely to get parsed right by browsers

--- site/popvox/templates/popvox/widgets/bill.js
+++ site/popvox/templates/popvox/widgets/bill.js
@@ -13,10 +13,10 @@ document.write('.popvoxwidget.wide .procon { color: #fff; font: 13px/21px helvet
 document.write('.popvoxwidget.wide .billtitle { color: #75747c; font: normal 10px/11px helvetica, sans-serif; font-weight: bold; background: none; margin: 0 0 3px 0; padding: 0; height: 22px; overflow: hidden; }\n');
 document.write('.popvoxwidget.wide .speakup { color: #CC6A11; font: bold 10px/14px helvetica,sans-serif; margin: 0; }\n');
 document.write('</style>');
-document.write('<a class="popvoxwidget {% if request.GET.title == "1" %}wide{% else %}small{% endif %}"' 
+document.write('<div><a class="popvoxwidget {% if request.GET.title == "1" %}wide{% else %}small{% endif %}"' 
 	+ ' style="background: url(https://www.popvox.com/media/icons/speak_up_{% if bill and stats %}ext{% else %}bubble{% endif %}{% if request.GET.title == "1" %}_wide{% endif %}.png) left top no-repeat;"'
 	+ ' href="{{siteroot}}{% if bill %}{{bill.url}}{% endif %}" {% if request.GET.iframe == '1' %}target="_blank"{% endif %}>'
 	+ '<div class="procon">{% if stats %}{{stats.pro_pct}}%<br/>{{stats.con_pct}}%{% endif %}</div>'
-	{% if request.GET.title == "1" %}+ '<div style="display: block; float: left; width: 180px; padding: 7px 0 0 14px;"><p class="billtitle">{{bill.title|truncatewords:15}}</p><p class="speakup">SPEAK UP at POPVOX</p></div>'{% endif %}
-	+ '</a>')
+	{% if request.GET.title == "1" %}+ '<div style="display: block; float: left; width: 180px; padding: 7px 0 0 14px;"><div class="billtitle">{{bill.title|truncatewords:15}}</div><div class="speakup">SPEAK UP at POPVOX</div></div>'{% endif %}
+	+ '</a></div>')
 
