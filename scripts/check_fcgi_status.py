2cf674910a53b7302aa1ece8e404ccf725f682b8
Joshua Tauberer <josh@popvox.com>
2012-02-14 14:57:06 +0000

    remove LOADING_DUMP_DATA from runscript

--- scripts/check_fcgi_status.py
+++ scripts/check_fcgi_status.py
@@ -16,11 +16,5 @@ ret = C().open("https://%s.popvox.com/?nocache=1" % os.environ["USER"])
 if ret.getcode() != 200:
 	print os.environ["USER"], "returned status code", ret.getcode()
 
-	# runscript sets this, which causes major problems because
-	# it prevents the User creation signal handler from executing
-	# which leaves our db in an inconsistent state of not creating
-	# UserProfile objects.
-	env = dict(os.environ)
-	del env["LOADING_DUMP_DATA"]
-
-	#subprocess.call(["./fcgi"], shell=True, env=env)
+	#if ret.getcode() == 502:
+	#	subprocess.call(["./fcgi"], shell=True)
