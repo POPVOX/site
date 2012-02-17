f7b7a75e09bbad33529b3193599dddd549da6585
Annalee Flower Horne <annalee@popvox.com>
2012-02-17 18:29:31 +0000

    added a notes field to ServiceAccountPermission

--- site/popvox/models.py
+++ site/popvox/models.py
@@ -1499,6 +1499,7 @@ class BillSimilarity(models.Model):
 
 class ServiceAccountPermission(models.Model):
 	name = models.CharField(max_length=20, unique=True)
+	notes = models.TextField(blank=True, null=True)
 	def __unicode__(self):
 		return self.name
 
