d4415c60dd0530ce75eded20a9f5e0154a9ed1b8
Annalee Flower Horne <annalee@popvox.com>
2012-03-12 19:16:44 +0000

    Added the new "about/testimonials" page. Created a template for the about pages' sidebar so that changes to the sidebar don't have to be hand-coded into every page, added the testimonials link to the sidebar, added two new documents to the sidebar (per Rachna's request).

--- site/urls.py
+++ site/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
 	(r'ajax/master-state', 'popvox.views.main.master_state'),
 	(r'ajax/get-short-url', 'popvox.views.main.get_short_url'),
 	
-	(r'^(|congress|congress/letters|organization|about|about/team|about/principles|about/whyitworks|about/contact|advertising|faq|blog_template|features/opendataday2011)$', 'popvox.views.main.staticpage'), # maps arg to a template file name without checking for safety, so options must be defined in the regex explicitly
+	(r'^(|congress|congress/letters|organization|about|about/team|about/principles|about/whyitworks|about/contact|about/testimonials|advertising|faq|blog_template|features/opendataday2011)$', 'popvox.views.main.staticpage'), # maps arg to a template file name without checking for safety, so options must be defined in the regex explicitly
 	(r'^press$', 'popvox.views.main.press_page'),
 	(r'^legal$', 'popvox.views.main.legal_page'),
 
