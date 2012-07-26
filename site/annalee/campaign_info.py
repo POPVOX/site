15860269db6a058385058a19582e428af5b187bc
Annalee Flower Horne <annalee@popvox.com>
2012-07-26 02:02:33 +0000

    add positions to user_data, add more user data to campaign_info

--- site/annalee/campaign_info.py
+++ site/annalee/campaign_info.py
@@ -1,9 +1,39 @@
+#!runscript
+
 # (Paste this into a ./manage shell)
 # It'll make a file in the dir you ran it from called campaign_info.csv
 
 import popvox.models as pv
+from django.contrib.auth.models import User
+import unicodedata
+
+sep = "\t"
 
 with open('campaign_info.csv','w') as info:
-        for campaign in pv.ServiceAccountCampaign.objects.filter(account__id=1926):
-                    for record in campaign.actionrecords.all():
-                                    info.write( str(record.firstname) +","+ str(record.lastname) +","+ str(record.zipcode) +","+ str(record.email) +","+ str(record.created) +","+ str(record.updated) +","+ str(record.completed_stage) ) +"\n"
+    for campaign in pv.ServiceAccountCampaign.objects.filter(account__id=1196):
+        for record in campaign.actionrecords.all():
+            try:
+                user = User.objects.get(email=record.email)
+                lastcomment = pv.UserComment.objects.filter(user=user).latest('created')
+                if lastcomment:
+                    state = lastcomment.state
+                    dist = lastcomment.congressionaldistrict
+                    statedist = str(state)+sep+str(dist)
+                else:
+                    statedist = sep
+            except:
+                statedist = sep
+                
+            if record.completed_comment:
+                message = record.completed_comment.message
+                if message:
+                    message = unicodedata.normalize('NFKD',message).encode('ascii','ignore').replace("\n"," ")
+                    message = message.replace("\r","")
+            else:
+                message = sep
+            
+            info.write( str(record.firstname) +sep+ str(record.lastname) +sep+ statedist +sep+ str(record.zipcode) +sep+ str(record.email) +sep+ str(record.created) +sep+ str(record.updated) +sep+ str(record.completed_stage)+ sep +str(message) +"\n" )
+
+            
+#info.write( str(record.firstname) +","+ str(record.lastname) +","+ statedist +","+ str(record.zipcode) +","+ str(record.email) +","+ str(record.created) +","+ str(record.updated) +","+ str(record.completed_stage) ) +"\n"
+            
