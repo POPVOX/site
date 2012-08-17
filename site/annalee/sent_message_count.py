33a9b6c557b4df380b4ec0b5178eb935eed9a0d7
Mackenzie Morgan <mackenzie@popvox.com>
2012-08-17 02:27:48 +0000

    new script to count up how many messages an org has sent through popvox in a given month

---
+++ site/annalee/sent_message_count.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# Use this to get a count of how many comments an org has sent in a given month
+import datetime
+import popvox.models as pv
+count = 0
+org = pv.Org.objects.get(name__contains="Heritage")
+campaigns = pv.ServiceAccountCampaign.objects.filter(account=org.service_account)
+for campaign in campaigns:
+        ars = campaign.actionrecords.filter(created__year='2012',created__month='7')
+            count += len(ars)
+            print count
+
+
