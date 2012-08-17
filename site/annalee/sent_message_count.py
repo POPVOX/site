85e7e4b0de4b0383dfad4c8584609d4de9477280
Mackenzie Morgan <mackenzie@popvox.com>
2012-08-17 02:32:47 +0000

    only include completed comments

--- site/annalee/sent_message_count.py
+++ site/annalee/sent_message_count.py
@@ -6,7 +6,7 @@ count = 0
 org = pv.Org.objects.get(name__contains="Heritage")
 campaigns = pv.ServiceAccountCampaign.objects.filter(account=org.service_account)
 for campaign in campaigns:
-        ars = campaign.actionrecords.filter(created__year='2012',created__month='7')
+        ars = campaign.actionrecords.filter(created__year='2012',created__month='7',completed_stage='finished')
             count += len(ars)
             print count
 
