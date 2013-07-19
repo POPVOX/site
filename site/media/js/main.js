d157169ef2032a7798d8ca6074a8bae6e835ae63
William Donnell <william@popvox.com>
2013-07-19 15:22:34 +0000

    updates to the leg agenda widget.

--- site/media/js/main.js
+++ site/media/js/main.js
@@ -74,6 +74,20 @@ POP = function()
 				text: 'You have already taken action.'
 			}
 		});
+		$('.proposal .sentiment').qtip({
+			style: {
+				classes: 'qtip-dark qtip-shadow'
+			},
+			position: {
+				my: 'bottom center',
+				at: 'top center'
+			},
+			content: {
+				text: function(api) {
+					return $(this).find('.tooltip_sent').html();
+				}
+			}
+		});
 	};
 
 	var makeInactive = function(billID)
