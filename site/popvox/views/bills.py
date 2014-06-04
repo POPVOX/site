3f06d87bd8f592b40e6f05d421e116769c8dc1c1
bryandease <homer5050>
2014-06-04 13:41:27 -0500

    Testing loading of regulation comments

--- site/popvox/views/bills.py
+++ site/popvox/views/bills.py
@@ -2374,7 +2374,8 @@ def regreport(request, agency, regnumber):
 
 def regreport_comments(request, agency, regnumber, start):
     regulation = get_object_or_404(Regulation, agency=agency, regnumber=regnumber)
-    comments = regulation.usercomments.filter(message__isnull = False, status__in=(UserComment.COMMENT_NOT_REVIEWED, UserComment.COMMENT_ACCEPTED))[start:21]
+    end = int(start) + 10
+    comments = regulation.usercomments.filter(message__isnull = False, status__in=(UserComment.COMMENT_NOT_REVIEWED, UserComment.COMMENT_ACCEPTED))[start:end]
 
     return render_to_response('popvox/regulation_comments.html', {
         "comments": comments,
